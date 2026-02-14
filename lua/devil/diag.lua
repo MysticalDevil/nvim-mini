a9c3e9e2f62e63f03f0f6266eff22e1951946ee4
MysticalDevil <jinpeng7981@outlook.com>
2026-02-14 12:33:57 +0800

    fix(diag): define diagnostic sign text map

--- lua/devil/diag.lua
+++ lua/devil/diag.lua
@@ -1,8 +1,8 @@
 local signs = {
-  [vim.diagnostic.severity.ERROR] = "󰅚 ",
-  [vim.diagnostic.severity.WARN] = " ",
-  [vim.diagnostic.severity.HINT] = "󰌶 ",
-  [vim.diagnostic.severity.INFO] = " ",
+  [vim.diagnostic.severity.ERROR] = "E",
+  [vim.diagnostic.severity.WARN] = "W",
+  [vim.diagnostic.severity.INFO] = "I",
+  [vim.diagnostic.severity.HINT] = "H",
 }
 
 local opts = {
