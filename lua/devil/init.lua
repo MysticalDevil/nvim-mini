4a6ad8f93282c3b45922ad52557f75f220e78d49
MysticalDevil <jinpeng7981@outlook.com>
2026-02-14 12:49:41 +0800

    feat(local): support optional machine-specific local config

--- lua/devil/init.lua
+++ lua/devil/init.lua
@@ -3,6 +3,7 @@ require("devil.plug").setup()
 require("devil.lsp").setup()
 require("devil.diag").setup()
 require("devil.help").setup()
+pcall(require, "devil.local")
 
 local utils = require("devil.utils")
 utils.set_keymaps(require("devil.keymap").general)
---
+++ lua/devil/local.example.lua
@@ -0,0 +1,5 @@
+-- Copy this file to `lua/devil/local.lua` for machine-specific tweaks.
+-- Keep sensitive values and host-specific settings out of shared config.
+
+-- Example:
+-- vim.opt.guifont = "FiraCode Nerd Font:h13"
