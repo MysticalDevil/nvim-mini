5f0b49351190755e5843026673bf5d83af0d8bfd
MysticalDevil <jinpeng7981@outlook.com>
2026-02-14 12:48:04 +0800

    feat(plug): add on-demand PackSync and TSInstallAll commands

--- lua/devil/plug.lua
+++ lua/devil/plug.lua
@@ -2,6 +2,10 @@ local utils = require("devil.utils")
 
 local M = {}
 
+local ts_parsers = {
+  "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust", "zig", "go",
+}
+
 local function plug_install()
   utils.plug_add({
     "nvim-treesitter/nvim-treesitter",
@@ -25,14 +29,26 @@ local function plug_setting()
   })
 
   require("nvim-treesitter").setup()
-  require("nvim-treesitter").install({
-    "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust", "zig", "go"
-  }):wait(300000)
+end
+
+local function install_treesitter_parsers()
+  require("nvim-treesitter").install(ts_parsers)
+end
+
+local function setup_user_commands()
+  vim.api.nvim_create_user_command("PackSync", function()
+    plug_install()
+  end, { desc = "Install or sync configured vim.pack plugins" })
+
+  vim.api.nvim_create_user_command("TSInstallAll", function()
+    install_treesitter_parsers()
+  end, { desc = "Install configured Treesitter parsers" })
 end
 
 function M.setup()
   plug_install()
   plug_setting()
+  setup_user_commands()
   local ok = pcall(vim.cmd.colorscheme, "onedark")
   if not ok then
     vim.cmd.colorscheme("habamax")
